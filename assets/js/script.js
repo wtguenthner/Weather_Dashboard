1ad2b829c59ce74b2f992acda2eae83657c5c550
Taylor Guenthner <wtguenthner@gmail.com>
2022-06-06 19:10:21 -0400

    Displays current weather of selected city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 
 var searchFormEl = document.querySelector('#searchForm');
 var cityDisplay = document.querySelector('#cityList');
+var currentDisplay = document.querySelector('.current');
 // var store = eval(localStorage.cities) || [];
 // var store = localStorage.getItem('cities') || [];
 document.querySelector('button').addEventListener('click', handleClick);
@@ -23,7 +24,7 @@ localStorage.setItem('cities', JSON.stringify(cityList));
 
 
 
-displayCity();
+
 
 let url= `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
 
@@ -35,13 +36,16 @@ fetch(url).then(data=>data.json()).then(data => {
   let url2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
   fetch(url2).then(data=>data.json()).then(data => {
 
-    let { current, daily } = data;
     jsonData = data;
     console.log(data)
+    displayCity();
+    displayCurrent(jsonData,city);
   })
 })
 
+
 }
+
 
 function displayCity(){
   cityDisplay.innerHTML = " "
@@ -49,7 +53,7 @@ cityList = JSON.parse(localStorage.getItem('cities'))
 if(cityList!=null){
 for(var x =0; x < cityList.length; x++){
 
-  cityDisplay.innerHTML += `<p class = button>` + cityList[x] +`</p>`;
+  cityDisplay.innerHTML += `<button class=button onclick="displayCurrent(jsonData[x],cityList[x])">` + cityList[x] +`</button>`;
 }
 }
 }
@@ -57,4 +61,19 @@ for(var x =0; x < cityList.length; x++){
 function clearStorage(){
 
   localStorage.clear();
+}
+
+function displayCurrent(jsonData, city){
+
+  currentDisplay.innerHTML = `<h3>` +city.toUpperCase() + `</h3>`;
+  currentDisplay.innerHTML += `<p>Temp: ${jsonData.current.temp}</p>` ;
+  currentDisplay.innerHTML += `<p>Wind: ${jsonData.current.wind_speed} MPH</p>`;
+  currentDisplay.innerHTML += `<p>Humidity: ${jsonData.current.humidity}%</p>`;
+  if(jsonData.current.uvi < 2){
+  currentDisplay.innerHTML += `<p>UV Index: <span style="background-color:green">${jsonData.current.uvi}</span</p>`;
+  }else if(jsonData.current.uvi > 7 ){
+    currentDisplay.innerHTML += `<p>UV Index: <span style="background-color:red">${jsonData.current.uvi}</span</p>`;
+  }else{
+    currentDisplay.innerHTML += `<p>UV Index: <span style="background-color:yellow">${jsonData.current.uvi}</span</p>`;
+  }
 }
