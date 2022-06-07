4872d3732b9867aaa8ebaa9c3166277477f886d1
Taylor Guenthner <wtguenthner@gmail.com>
2022-06-07 14:22:26 -0400

    Fixed a url error

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,7 @@ var cityList = JSON.parse(localStorage.getItem('cities'));
 cityList.push(city);
 localStorage.setItem('cities', JSON.stringify(cityList));
 
-let url= `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
+let url= `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
 
 fetch(url).then(data=>data.json()).then(data => {
 
@@ -81,7 +81,7 @@ const parts = {
     };
 
   //Displays the current day of the city searched
-  currentDisplay.innerHTML = `<h3>` + city.toUpperCase() +  " " + `(${parts.month+1}/` + parts.date + `/${parts.year})` +`<img src="http://openweathermap.org/img/wn/${icon}@2x.png" width="80" height="80" alt="${weatherDescr}" ></h3> `;
+  currentDisplay.innerHTML = `<h3>` + city.toUpperCase() +  " " + `(${parts.month+1}/` + parts.date + `/${parts.year})` +`<img src="https://openweathermap.org/img/wn/${icon}@2x.png" width="80" height="80" alt="${weatherDescr}" ></h3> `;
   currentDisplay.innerHTML += `<p>Temp: ${jsonData.current.temp}</p>` ;
   currentDisplay.innerHTML += `<p>Wind: ${jsonData.current.wind_speed} MPH</p>`;
   currentDisplay.innerHTML += `<p>Humidity: ${jsonData.current.humidity}%</p>`;
@@ -103,31 +103,31 @@ const parts = {
   var iconFive = jsonData.daily[4].weather[0].icon
 
   //Sets day one of the forecast
-  forecastOne.innerHTML = `<h3>${parts.month+1}/` + `${parts.date+1}` + `/${parts.year}</h3>` + `<img src="http://openweathermap.org/img/wn/${iconOne}@2x.png" width="60" height="60">` 
+  forecastOne.innerHTML = `<h3>${parts.month+1}/` + `${parts.date+1}` + `/${parts.year}</h3>` + `<img src="https://openweathermap.org/img/wn/${iconOne}@2x.png" width="60" height="60">` 
   forecastOne.innerHTML += `<p>Temp: ${jsonData.daily[0].temp.day}</p>`
   forecastOne.innerHTML += `<p>Wind: ${jsonData.daily[0].wind_speed} MPH</p>`
   forecastOne.innerHTML += `<p>Humidity: ${jsonData.daily[0].humidity}</p>`
 
   //Sets day two of the forecast
-  forecastTwo.innerHTML = `<h3>${parts.month+1}/` + `${parts.date+2}` + `/${parts.year}</h3>` + `<img src="http://openweathermap.org/img/wn/${iconTwo}@2x.png" width="60" height="60">` 
+  forecastTwo.innerHTML = `<h3>${parts.month+1}/` + `${parts.date+2}` + `/${parts.year}</h3>` + `<img src="https://openweathermap.org/img/wn/${iconTwo}@2x.png" width="60" height="60">` 
   forecastTwo.innerHTML += `<p>Temp: ${jsonData.daily[1].temp.day}</p>`
   forecastTwo.innerHTML += `<p>Wind: ${jsonData.daily[1].wind_speed} MPH</p>`
   forecastTwo.innerHTML += `<p>Humidity: ${jsonData.daily[1].humidity}</p>`
 
   //Sets day three of the forecast
-  forecastThree.innerHTML = `<h3>${parts.month+1}/` + `${parts.date+3}` + `/${parts.year}</h3>` + `<img src="http://openweathermap.org/img/wn/${iconThree}@2x.png" width="60" height="60">` 
+  forecastThree.innerHTML = `<h3>${parts.month+1}/` + `${parts.date+3}` + `/${parts.year}</h3>` + `<img src="https://openweathermap.org/img/wn/${iconThree}@2x.png" width="60" height="60">` 
   forecastThree.innerHTML += `<p>Temp: ${jsonData.daily[2].temp.day}</p>`
   forecastThree.innerHTML += `<p>Wind: ${jsonData.daily[2].wind_speed} MPH</p>`
   forecastThree.innerHTML += `<p>Humidity: ${jsonData.daily[2].humidity}</p>`
 
   //Sets day four of the forecast
-  forecastFour.innerHTML = `<h3>${parts.month+1}/` + `${parts.date+4}` + `/${parts.year}</h3>` + `<img src="http://openweathermap.org/img/wn/${iconFour}@2x.png" width="60" height="60">` 
+  forecastFour.innerHTML = `<h3>${parts.month+1}/` + `${parts.date+4}` + `/${parts.year}</h3>` + `<img src="https://openweathermap.org/img/wn/${iconFour}@2x.png" width="60" height="60">` 
   forecastFour.innerHTML += `<p>Temp: ${jsonData.daily[3].temp.day}</p>`
   forecastFour.innerHTML += `<p>Wind: ${jsonData.daily[3].wind_speed} MPH</p>`
   forecastFour.innerHTML += `<p>Humidity: ${jsonData.daily[3].humidity}</p>`
 
   //Sets day five of the forecast
-  forecastFive.innerHTML = `<h3>${parts.month+1}/` + `${parts.date+5}` + `/${parts.year}</h3>` + `<img src="http://openweathermap.org/img/wn/${iconFive}@2x.png" width="60" height="60">` 
+  forecastFive.innerHTML = `<h3>${parts.month+1}/` + `${parts.date+5}` + `/${parts.year}</h3>` + `<img src="https://openweathermap.org/img/wn/${iconFive}@2x.png" width="60" height="60">` 
   forecastFive.innerHTML += `<p>Temp: ${jsonData.daily[4].temp.day}</p>`
   forecastFive.innerHTML += `<p>Wind: ${jsonData.daily[4].wind_speed} MPH</p>`
   forecastFive.innerHTML += `<p>Humidity: ${jsonData.daily[4].humidity}</p>`
@@ -138,7 +138,7 @@ const parts = {
 function displayPrevious(value){
 
   var city = value;
-  let url= `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
+  let url= `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
 
   fetch(url).then(data=>data.json()).then(data => {
 
